6e10d013b6e24dada68230f4fae907f8ea50a924
omerfiraat <omerfiraatx@gmail.com>
2023-03-20 14:14:41 +0300

    Publish

---
+++ Feeltune/Constants.swift
@@ -0,0 +1,38 @@
+//
+//  Constants.swift
+//  Feeltune
+//
+//  Created by Ömer Firat on 13.03.2023.
+//
+
+import Foundation
+
+let accessTokenKey = "access-token-key"
+let redirectUri = URL(string:"feeltune://")!
+let spotifyClientId = "7650fc01123143e2bcf04fe1d274605a"
+let spotifyClientSecretKey = "0044ecb0f822450a8ee5b1054a48dc94"
+
+/*
+Scopes let you specify exactly what types of data your application wants to
+access, and the set of scopes you pass in your call determines what access
+permissions the user is asked to grant.
+For more information, see https://developer.spotify.com/web-api/using-scopes/.
+*/
+let scopes: SPTScope = [
+                            .userReadEmail, .userReadPrivate,
+                            .userReadPlaybackState, .userModifyPlaybackState, .userReadCurrentlyPlaying,
+                            .streaming, .appRemoteControl,
+                            .playlistReadCollaborative, .playlistModifyPublic, .playlistReadPrivate, .playlistModifyPrivate,
+                            .userLibraryModify, .userLibraryRead,
+                            .userTopRead, .userReadPlaybackState, .userReadCurrentlyPlaying,
+                            .userFollowRead, .userFollowModify,
+                        ]
+let stringScopes = [
+                        "user-read-email", "user-read-private",
+                        "user-read-playback-state", "user-modify-playback-state", "user-read-currently-playing",
+                        "streaming", "app-remote-control",
+                        "playlist-read-collaborative", "playlist-modify-public", "playlist-read-private", "playlist-modify-private",
+                        "user-library-modify", "user-library-read",
+                        "user-top-read", "user-read-playback-position", "user-read-recently-played",
+                        "user-follow-read", "user-follow-modify",
+                    ]
